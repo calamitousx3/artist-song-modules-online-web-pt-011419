31cec57dea948cfe4b3263e4dbd4a3157ae00ae6
Carmen Renee <carmengenpt@gmail.com>
2019-02-24 16:46:29 +0000

    Automatically backed up by Learn

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,25 @@
+module Findable
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect{|o| o.name}
+    end    
+  end
+end
+# //////////////////////////////////////////
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
